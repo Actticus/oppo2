964932c6df9206895417774962f07967574c84a0
saltytimofey <saltytimofey@gmail.com>
2020-11-21 00:17:16 +0700

    bug_fix

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
-double my_pow(double x, unsigned int y){
+double my_pow(double x, int y){
     if (y == 0)
         return 1;
-    for (int i = 1; i < y; i++)
-    {
-        x *= 2;
+    if (y > 0)
+        for (int i = 1; i < y; i++)
+        {
+            x *= 2;
+        }
+    if (y < 0){
+        for (int i = y; i < -1; i++)
+            {
+                x *= 2;
+            }
+        x = 1 / x;
     }
     return x;
 }
